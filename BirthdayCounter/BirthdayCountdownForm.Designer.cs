37c8555ed665c2ba6e1fa2a413a0dc788ede1941
Allon Hadaya <allonhadaya@hotmail.com>
2013-03-27 17:08:09 -0400

    Commit minimal birthday counter with ignore file.

---
+++ BirthdayCounter/BirthdayCountdownForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace BirthdayCounter
+{
+    partial class BirthdayCountdownForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.birthdayPicker = new System.Windows.Forms.DateTimePicker();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.labelUpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // birthdayPicker
+            // 
+            this.birthdayPicker.CustomFormat = "";
+            this.birthdayPicker.Dock = System.Windows.Forms.DockStyle.Top;
+            this.birthdayPicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.birthdayPicker.Location = new System.Drawing.Point(0, 0);
+            this.birthdayPicker.Name = "birthdayPicker";
+            this.birthdayPicker.Size = new System.Drawing.Size(324, 20);
+            this.birthdayPicker.TabIndex = 0;
+            this.birthdayPicker.ValueChanged += new System.EventHandler(this.birthdayPicker_ValueChanged);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.countdownLabel.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.Location = new System.Drawing.Point(0, 20);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(324, 47);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.Text = "pick your birthday";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelUpdateTimer
+            // 
+            this.labelUpdateTimer.Enabled = true;
+            this.labelUpdateTimer.Interval = 1000;
+            this.labelUpdateTimer.Tick += new System.EventHandler(this.labelUpdateTimer_Tick);
+            // 
+            // BirthdayCountdownForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 67);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.birthdayPicker);
+            this.Name = "BirthdayCountdownForm";
+            this.Text = "Birthday Countdown";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker birthdayPicker;
+        private System.Windows.Forms.Label countdownLabel;
+        private System.Windows.Forms.Timer labelUpdateTimer;
+
+    }
+}
+
---
+++ BirthdayCounter/BirthdayCountdownForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BirthdayCounter
+{
+    public partial class BirthdayCountdownForm : Form
+    {
+        private DateTime? Birthday;
+
+        public BirthdayCountdownForm()
+        {
+            InitializeComponent();
+        }
+
+        private void birthdayPicker_ValueChanged(object sender, EventArgs e)
+        {
+            Birthday = birthdayPicker.Value.Date;
+            UpdateCountdownLabel();
+        }
+
+        private void labelUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateCountdownLabel();
+        }
+
+        private void UpdateCountdownLabel()
+        {
+            if (Birthday.HasValue) {
+                countdownLabel.Text = (Birthday.Value - DateTime.Now).ToString(@"d\ \d\a\y\s\ h\:m\:ss");
+            } else {
+                countdownLabel.Text = "pick your birthday";
+            }
+        }
+    }
+}
